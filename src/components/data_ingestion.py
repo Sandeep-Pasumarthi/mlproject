5c1a20bc7f16f382410720f11a9862e12880d4e7
Sandeep-Pasumarthi <sivasandeeppasumarthi@gmail.com>
2024-04-21 19:33:26 +0530

    doc strings added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -16,10 +16,44 @@ class DataIngestionConfig:
 
 
 class DataIngestion:
+    """
+    Class for data ingestion.
+
+    Args:
+        config (DataIngestionConfig): Configuration object containing paths for raw, train and test data.
+
+    Attributes:
+        config (DataIngestionConfig): Configuration object containing paths for raw, train and test data.
+
+    Methods:
+        initiate_data_ingestion(self): Initiates the data ingestion process.
+    """
     def __init__(self, config: DataIngestionConfig) -> None:
+        """
+        Initializes the DataIngestion class with the provided configuration.
+
+        Args:
+            config (DataIngestionConfig): Configuration object containing paths for raw, train and test data.
+
+        Attributes:
+            config (DataIngestionConfig): Configuration object containing paths for raw, train and test data.
+        """
         self.config = config
     
     def initiate_data_ingestion(self):
+        """
+        Initiates the data ingestion process.
+
+        Args:
+            None
+
+        Returns:
+            tuple: Tuple containing paths of train and test data.
+
+        Raises:
+            CustomException: If any exception occurs during the data ingestion process.
+
+        """
         try:
             logging.info("Starting data ingestion")
             df = pd.read_csv(self.config.raw_data_path)
@@ -31,9 +65,6 @@ class DataIngestion:
             train.to_csv(self.config.train_data_path, index=False)
             test.to_csv(self.config.test_data_path, index=False)
             logging.info(f"Splitted the data into train and test")
-            return (
-                self.config.train_data_path,
-                self.config.test_data_path
-            )
+            return self.config.train_data_path, self.config.test_data_path
         except Exception as e:
             raise CustomException(str(e), sys)
